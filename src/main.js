87aa755edeb1f351a38be44e276587f8083f00e9
Jan Noel S. Paed <jannoelpaed27@gmail.com>
2025-07-30 20:36:06 +0800

    resizeable

--- src/main.js
+++ src/main.js
@@ -62,4 +62,9 @@ function animate() {
   controls.update();
   renderer.render(scene, camera);
 }
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
 animate();
